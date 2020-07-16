c2d73e06d37c7bf7c1cc6067f8c73b7d807b9068
Readiz <readiz@readiz.com>
2020-07-16 11:42:39 +0900

    --- Auto-Git Commit ---

--- step_09/8-1011.cpp
+++ step_09/8-1011.cpp
@@ -1,10 +1,22 @@
 #include <stdio.h>
-#define MEMO_MAX 100000
+#define MEMO_MAX 100000000
 int main() {
     int T;
     scanf("%d", &T);
     for (int tc = 0; tc < T; tc++) {
-        int memo[MEMO_MAX]
+        unsigned int memo[MEMO_MAX] = { 0, 1, 2, 4, 6, 9, 12, 16, };
+        unsigned int x, y, diff;
+        scanf("%u %u", &x, &y);
+        diff = y - x;
+        for (int i = 1; i < MEMO_MAX; i++) {
+            if (memo[i] == 0) {
+                memo[i] = memo[i - 1] + (unsigned int)((i + 1) / 2);
+            }
+            if (memo[i] >= diff) {
+                printf("%d\n", i);
+                break;                
+            }
+        }
     }
     return 0;
 }
@@ -17,12 +29,12 @@ int main() {
 그럼 요 형태의 배열을 가지고 있으면 될 듯...? (최대거리가 되는거니)
 
 공간이동 장치 발동 수에 따른 max 거리
-1                => 1   (  1)
-1 1              => 2  ( + 1)
-1 2 1            => 4  ( + 2)
-1 2 2 1          => 6  ( + 2)
-1 2 3 2 1        => 9  ( + 3)
-1 2 3 3 2 1      => 12 ( + 3)
-1 2 3 4 3 2 1    => 16 ( + 4)
+1: 1                => 1   (  1) <- (i + 1) / 2 의 값
+2: 1 1              => 2  ( + 1)
+3: 1 2 1            => 4  ( + 2)
+4: 1 2 2 1          => 6  ( + 2)
+5: 1 2 3 2 1        => 9  ( + 3)
+6: 1 2 3 3 2 1      => 12 ( + 3)
+7: 1 2 3 4 3 2 1    => 16 ( + 4)
 
 */
