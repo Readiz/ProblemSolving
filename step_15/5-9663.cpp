714bb3391d754f5303babb2b19d12c411ea6fac9
Readiz <readiz@readiz.com>
2020-07-27 14:08:15 +0900

    --- Auto-Git Commit ---

--- step_15/5-9663.cpp
+++ step_15/5-9663.cpp
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 int static N;
-bool static board[8][8] = { { false, },  };
+bool static board[15][15] = { { false, },  };
 int static result = 0;
 
-void static copyBoard(bool source[8][8], bool target[8][8]) {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
+void static copyBoard(bool source[15][15], bool target[15][15]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             target[i][j] = source[i][j];
         }
     }
 }
 void static updateIfItIsPossible(int targetI, int targetJ) {
-    if (targetI >= 0 && targetI < 8 && targetJ >= 0 && targetJ < 8) {
+    if (targetI >= 0 && targetI < N && targetJ >= 0 && targetJ < N) {
         board[targetI][targetJ] = true;
     }
 }
 void static updateBoard(int targetI, int targetJ) {
     // 좌/우, 상/하
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < N; i++) {
         board[i][targetJ] = true;
     }
-    for (int j = 0; j < 8; j++) {
+    for (int j = 0; j < N; j++) {
         board[targetI][j] = true;
     }
     // 대각선
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < N; i++) {
         updateIfItIsPossible(targetI - i, targetJ - i);
         updateIfItIsPossible(targetI - i, targetJ + i);
         updateIfItIsPossible(targetI + i, targetJ - i);
@@ -35,8 +35,8 @@ void static updateBoard(int targetI, int targetJ) {
 
 void static printBoard() {
     printf("------------\n");
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             if (board[i][j]) printf("X");
             else printf("O");
         }
@@ -45,35 +45,38 @@ void static printBoard() {
     printf("------------\n");
 }
 
-void static calculateStep(int step) {
+void static calculateStep(int step, int startFrom) {
     if (step == N) { // 최종까지 왔을 경우
         result++; // 결과를 추가한다.
         return;
     }
-
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            // 추가로 현재 위치에 놓을 수 있을 때만 계산
-            if (board[i][j] == false) {
-                // 현 위치 백업
-                bool tmpBoard[8][8];
-                copyBoard(board, tmpBoard);
-                updateBoard(i, j);
-                printBoard();
-                // 다음 단계 계산
-                calculateStep(step + 1);
-                // 백업본으로 되돌리기
-                copyBoard(tmpBoard, board);
-            }
+    // printf("StartFrom: %d\n", startFrom);
+    for (int k = startFrom; k < N * N; k++) {
+        int i = k / N;
+        int j = k % N;
+        // printf("%d, %d called!\n", i, j);
+        // 추가로 현재 위치에 놓을 수 있을 때만 계산
+        if (board[i][j] == false) {
+            // 현 위치 백업
+            bool tmpBoard[15][15];
+            copyBoard(board, tmpBoard);
+            updateBoard(i, j);
+            // printBoard();
+            // 다음 단계 계산
+            calculateStep(step + 1, k + 1);
+            // 백업본으로 되돌리기
+            copyBoard(tmpBoard, board);
         }
     }
     return;
 }
 
 int main() {
+    int memo[16] = {0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 0, 0, 0, 0};
     scanf("%d", &N);
-    calculateStep(0);
+    calculateStep(0, 0);
     printf("%d\n", result);
+    // printf("%d\n", memo[N]);
     return 0;
 }
 
