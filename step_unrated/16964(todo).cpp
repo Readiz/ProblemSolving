4d06739a8889e11f1c479a78f7bd225fa1e55d09
Readiz <readiz@readiz.com>
2021-03-14 12:12:49 +0900

    0313

--- step_unrated/16964(todo).cpp
+++ step_unrated/16964(todo).cpp
@@ -21,6 +21,9 @@ int StackSize = 0;
 void push(int a) {
     Stack[StackSize++] = a;
 }
+int peek() {
+    return Stack[StackSize - 1];
+}
 int pop() {
     if (StackSize > 0) {
         return Stack[--StackSize];
@@ -32,36 +35,54 @@ void myAlloc(int x, int y) {
     if (e[x].headIdx == -1) { // 리스트에 처음 들어가는 경우
         e[x].headIdx = listSize;
         n[listSize].y = y;
+        // printf("%d is added on %d. head was empty!\n", y, x);
     } else {
         int idx = e[x].headIdx;
+        int count = 0;
         while (n[idx].nextIdx != -1) { // 마지막 아이템까지 탐색
             idx = n[idx].nextIdx;
+            count++;
         }
-        n[idx].y = y;
+        n[idx].nextIdx = listSize;
+        n[listSize].y = y;
+        // printf("%d is added on %d. ListSize: %d, Count: %d\n", y, x, listSize, count);
     }
     listSize++;
 }
+// x가 y랑 연결되면 true, 아니면 false.
 bool searchList(int x, int y) {
+    // printf("Search %d -> %d...\n", x, y);
     if (e[x].headIdx == -1) { // 해당 정점에 연결이 하나도 없는 경우
         return false;
     } else {
         int idx = e[x].headIdx;
-        while (n[idx].nextIdx != -1) {
+        do {
             if (n[idx].y == y) { // y를 찾았으면 리턴
+                // printf("Found! Result is true!\n");
                 return true;
             }
-            idx = n[idx].nextIdx;
+            idx = n[idx].nextIdx; // 다음 노드 연결 노드로 이동.
+        } while (n[idx].nextIdx != -1);
+        // 다음 idx가 -1 이어도 일단 여기까지는 찾긴 해야함
+        if (n[idx].y == y) { // y를 찾았으면 리턴
+            // printf("Found! Result is true!\n");
+            return true;
         }
     }
+    // printf("Not Found! Result is false!\n");
     return false;
 }
 
 bool dfs(int x) {
-    if (visitCount == N) {
+    if (visitCount == N) { // 전체 방문 했으면, true.
+        // printf("Finished the trip! Result: true!\n");
         return true;
     }
     for (int y = 1; y <= N; y++) {
-        if (!visited[y] && searchList(x, y)) {
+        if (visited[y]) continue;
+        // printf("[DFS] %d -> %d, search(x,y) = %d, userInput[%d] = %d\n", x, y, searchList(x, y), visitCount, userInput[visitCount]);
+        if (searchList(x, y) && userInput[visitCount] == y) {
+            // printf("Call DFS(%d)...\n", y);
             visitCount++;
             visited[y] = true;
             push(y);
@@ -76,6 +97,24 @@ bool dfs(int x) {
 }
 
 int main() {
+    // myAlloc(1, 2);
+    // myAlloc(1, 3);
+    // myAlloc(1, 4);
+    // myAlloc(1, 5);
+    // myAlloc(1, 6);
+    // myAlloc(2, 1);
+    // myAlloc(2, 3);
+    // myAlloc(2, 4);
+    // searchList(1, 2);
+    // searchList(1, 3);
+    // searchList(1, 6);
+    // searchList(1, 7);
+    // searchList(2, 1);
+    // searchList(2, 3);
+    // searchList(2, 4);
+    // searchList(2, 5);
+    // return 0;
+
     scanf("%d", &N);
     for (int i = 0; i < N - 1; i++) {
         int a, b;
@@ -89,15 +128,11 @@ int main() {
     visitCount = 1;
     visited[1] = true;
     push(1);
-    dfs(1);
-
-    for (int i = 0; i < N; i++) {
-        if (Stack[i] != userInput[i]) {
-            printf("0\n");
-            return 0;
-        }
+    if (userInput[0] == 1 && dfs(1)) { // 첫번째 입력은 무조건 1임
+        printf("1\n");
+    } else {
+        printf("0\n");
     }
-    printf("1\n");
     return 0;
 }
 
