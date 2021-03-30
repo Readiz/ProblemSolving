2c6fe8530dd653bf84e435bc5eeb865b8da0e427
Readiz <readiz@readiz.com>
2021-03-31 02:33:25 +0900

    2448

---
+++ boj_unrated/from20210330/2448.js
@@ -0,0 +1,137 @@
+const star = {
+    3: [
+        '  *  ',
+        ' * * ',
+        '*****'
+    ]
+};
+let minStar = 3;
+
+function solution(scanner) {
+    // 3, 6, 12, 24, 48, ... (3 * 2^N)
+    let N = scanner.readInt();
+    while (!star[N]) {
+        makeNextStar(minStar * 2);
+        minStar *= 2;
+    }
+    return star[N].join('\n');
+}
+
+function makeNextStar(N) {
+    const parentN = Math.floor(N / 2);
+    const newArr = [...star[String(parentN)], ...star[String(parentN)]];
+    let margin = '';
+    for (let i = 0; i < parentN; i++) {
+        margin += ' ';
+    }
+    for (let i = 0; i < parentN; i++) {
+        newArr[i] = margin + newArr[i] + margin;
+    }
+    for (let i = parentN; i < N; i++) {
+        newArr[i] += ' ' + newArr[i];
+    }
+    star[N] = newArr;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+// Readiz BOJ JS Util Start
+/////////////////////////////////////////////////////////////////////////////////
+(function readizInit() {
+    const isDebug = process.argv[2] === 'readizDebug';
+    const _input = (!isDebug) ?
+        require('fs').readFileSync('/dev/stdin') :
+        require('fs').readFileSync(process.argv[1].replace('.js', '_input.txt'));
+    let _index = 0;
+    const _index_max = _input.length;
+    const scanner = {
+        readInt: () => {
+            let result = 0;
+            let isMinus = false;
+            while ((!(_input[_index] & 0x10) && _input[_index] !== 45) && _index < _index_max) ++_index;
+            if (_input[_index] === 45) { isMinus = true; ++_index; }
+            while ((_input[_index] & 0x10) && _index < _index_max) { result = result * 10 + (_input[_index] & 0x0f); ++_index; }
+            return isMinus ? -result : result;
+        },
+        readString: () => {
+            while ((_input[_index] === 0x0a || _input[_index] === 0x20) && _index < _index_max) ++_index;
+            const startIdx = _index;
+            while (!(_input[_index] === 0x0a || _input[_index] === 0x20) && _index < _index_max) ++_index;
+            const endIdx = _index;
+            return _input.toString('utf-8', startIdx, endIdx);
+        },
+        readLine: () => {
+            while ((_input[_index] === 0x0a) && _index < _index_max) ++_index;
+            const startIdx = _index;
+            while (!(_input[_index] === 0x0a) && _index < _index_max) ++_index;
+            const endIdx = _index;
+            return _input.toString('utf-8', startIdx, endIdx);
+        }
+    }
+    if (!isDebug) {
+        global.console = {
+            log: () => {},
+            warn: () => {},
+            info: () => {},
+            debug: () => {},
+            error: () => {}
+        }
+        const answer = solution(scanner);
+        process.stdout.write(answer + '\n');
+    } else {
+        while (_index !== _index_max) {
+            process.stdout.write('--------------------------------------------\n');
+            process.stdout.write('New Start + Log\n');
+            process.stdout.write('--------------------------------------------\n');
+            const start = _index;
+            const answer = solution(scanner);
+            const end = _index;
+            process.stdout.write('----------------------\n');
+            process.stdout.write('Input\n')
+            process.stdout.write('----------------------\n');
+            process.stdout.write(_input.toString('utf-8', start, end).trim() + '\n');
+            process.stdout.write('----------------------\n');
+            process.stdout.write('Answer\n')
+            process.stdout.write('----------------------\n');
+            process.stdout.write(answer + '\n');
+        }
+    }
+})();
+
+/*
+예제 입력 1 
+24
+예제 출력 1 
+                       *                        
+                      * *                       
+                     *****                      
+                    *     *                     
+                   * *   * *                    
+                  ***** *****                   
+                 *           *                  
+                * *         * *                 
+               *****       *****                
+              *     *     *     *               
+             * *   * *   * *   * *              
+            ***** ***** ***** *****             
+           *                       *            
+          * *                     * *           
+         *****                   *****          
+        *     *                 *     *         
+       * *   * *               * *   * *        
+      ***** *****             ***** *****       
+     *           *           *           *      
+    * *         * *         * *         * *     
+   *****       *****       *****       *****    
+  *     *     *     *     *     *     *     *   
+ * *   * *   * *   * *   * *   * *   * *   * *  
+***** ***** ***** ***** ***** ***** ***** *****
+
+
+     *                        
+    * *                       
+   *****                      
+  *     *                     
+ * *   * *                    
+***** *****                   
+
+*/
